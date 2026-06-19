d384cc09a2e27d946aed201d6ce9fe4c96a02b53
hacker-navi <pulavarthinaveen27@gmail.com>
2026-06-19 19:04:35 +0530

    Duplicates removed

--- java/DecompileClassExample.java
+++
@@ -1,9 +0,0 @@
-public class DecompileClassExample {
-    public static void main(String[] args) {
-        System.out.println(new DecompileClassExample().buildMessage("decompiler"));
-    }
-
-    String buildMessage(String toolName) {
-        return "Compile this class and inspect it with a decompiler: " + toolName;
-    }
-}
